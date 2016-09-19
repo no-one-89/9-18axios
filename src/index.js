a3b011a41dd75a518b0bc92c1a9690a0c0af9e7d
no-one-89 <67049155@qq.com>
2016-09-19 17:22:28 +0800

    finish

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routers from './routes';
-import 'highlight.js/styles/github-gist.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import './main.css';
-// import Marked from './Marked'
-// ReactDOM.render(<Marked />, document.getElementById('app'));
-ReactDOM.render(<Routers />, document.getElementById('app'));
